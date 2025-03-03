1e2bb9074251733413f1ebb54cf7ee9226552ae4
Victor Wheeler <vaw_info998@comcast.net>
2025-03-03 09:58:23 -0700

    feat:  add test_doxygen_config.py unit test + test Doxyfile

---
+++ tests/test_doxygen_config.py
@@ -0,0 +1,21 @@
+import unittest
+import filecmp
+import os
+import sys
+sys.path.append("../src")
+from doxygen_config import DoxygenConfig  # NoQA
+
+
+class DoxygenConfigTest(unittest.TestCase):
+    def test_doxygen_config_binary_match_output(self):
+        cfg = DoxygenConfig()
+        f1 = 'Doxyfile'
+        f2 = 'Doxyfile_to_compare'
+        cfg.load(f1)
+        cfg.save(f2)
+        self.assertTrue(filecmp.cmp(f1, f2))
+        os.remove(f2)
+
+
+if __name__ == '__main__':
+    unittest.main()
